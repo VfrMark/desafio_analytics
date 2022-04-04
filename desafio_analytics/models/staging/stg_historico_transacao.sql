7fba61a106ea85d12a80d263552f79399d8720f4
Jessica Oliveira <jesslira23@gmail.com>
2022-04-04 10:57:53 -0300

    ajustes bigquery adicao historico_archive

---
+++ desafio_analytics/models/staging/stg_historico_transacao.sql
@@ -0,0 +1,22 @@
+with 
+    source as (
+        select *
+        from {{ source('analytics', 'raw_transactionhistory') }}
+    )
+    , transformed as (
+        select
+            transactionid as id_transacao
+            , productid as id_produto
+            , cast(transactiondate as timestamp) as data_transacao
+            , case 
+                when transactiontype = 'W' then 'ordem servico'
+                when transactiontype = 'S' then 'ordem venda'
+                when transactiontype = 'P' then 'ordem compra'
+            end as tipo_transacao
+            , quantity as qtd_produto
+            , actualcost as custo_produto
+            , cast(modifieddate as timestamp) as data_modificacao
+        from source
+    )
+select *
+from transformed
---
+++ desafio_analytics/tests/test_fact_historico_transacao_soma_quantidade.sql
@@ -0,0 +1,21 @@
+/* This test ensures that the sum of quantity of the "fact_historico_transacao" 
+in March 2010 does not change over time*/
+
+with
+    data as (
+        select 
+	        sum(qtd_produto) as qnt_produto 
+        from {{ ref('fact_historico_transacao') }}
+        where 
+            tipo_transacao = 'ordem servico' 
+	        and data_transacao between '2013-08-01 00:00:00 UTC' and '2013-08-30 00:00:00 UTC'
+    )
+    , validation as (
+        select *
+        from data
+        where
+            qnt_produto != 234509
+    )
+select *
+from validation
+
